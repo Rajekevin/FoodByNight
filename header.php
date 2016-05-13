e394f0190c14b3fd6eb1432cd963c85b43248d69
Rajekevin <rajekevin@hotmail.fr>
2016-05-13 20:57:19 +0200

    header.php added

--- header.php
+++ header.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="fr">
 	<head>
 		<meta charset="utf-8"/>
 		<title><?php bloginfo('title')?></title>
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
-		<?php wp_head() ?>
+		<?php wp_head(); ?>
+		<?php wp_enqueue_script("jquery"); ?>
+
+		<meta name="description" content="">
+		<meta name="author" content="">
+
 	</head>
 
 
 <header>
 	<h1><a href="<?php echo home_url('/')?>"><?php bloginfo('name') ?></H1>
+
+
+<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </header>
 
 
@@ -21,9 +29,7 @@
 		?>
 
 <nav>
-	<?php wp_nav_menu(array(
-	'menu'=>'primary'
-	)); ?>
+	<?php wp_nav_menu(); ?>
 </nav>
 
 	<div id="container">
