568b792363bff76ca4698a400d1efd890cf73cc3
Rajekevin <rajekevin@hotmail.fr>
2016-06-03 11:55:05 +0200

    MAJ Page.php

--- page.php
+++ page.php
@@ -3,10 +3,18 @@
 
 <div id="left">
 
-	<?php while(have_posts()): the_post()?>
+	<?php 
+
+	 query_posts( array (
+        'post_type' => 'Menu_FBN',
+        'posts_per_page' => -1
+        ) );
+
+	while(have_posts()): the_post()?>
 
 	<h2> <?php the_title(); ?></h2>
 	<?php the_content(); ?>
+	<?php the_post_thumbnail('medium') ?>
 
 	<?php endwhile; ?>
 
