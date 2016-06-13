02e0dec8fb62a3d61be3ce127f52a87341422c1c
Rajekevin <rajekevin@hotmail.fr>
2016-06-13 11:14:14 +0200

    Maj page

--- page.php
+++ page.php
@@ -19,8 +19,36 @@
 	<?php endwhile; ?>
 
 
-</div>
+<!-- </div> -->
 
 
 
 <?php get_footer() ?>
+
+
+
+// Loop for Portfolio
+<?php get_header(); ?>
+ 
+ 
+<div id="primary">
+    <div id="content" class="clearfix">
+        <?php
+ 
+        query_posts( array (
+        'post_type' => 'japonais',
+        'posts_per_page' => -1
+        ) );
+ 
+        while ( have_posts() ) : the_post(); ?>
+ 
+        <h2><?php the_title() ?></h2>
+        <?php the_post_thumbnail('medium') ?>
+ 
+			<?php endwhile; ?>
+ 
+    </div><!-- #content -->
+</div><!-- #primary -->
+ 
+ 
+<?php get_footer(); ?>
