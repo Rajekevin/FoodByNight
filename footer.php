c783bfbe16ed39b0a736f914deaf8b48a0ae2f99
Rajekevin <rajekevin@hotmail.fr>
2016-06-22 15:33:32 +0200

    footer.php

--- footer.php
+++ footer.php
@@ -11,7 +11,18 @@
 	</section>
 
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="assets/js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>
+<script src="assets/js/jquery.nav.js" type="text/javascript"></script>  
+<script src="assets/js/jquery.isotope.min.js" type="text/javascript"></script> 
 
+<script>
+$(document).ready(function(){
+    $(".nav-tabs a").click(function(){
+        $(this).tab('show');
+    });
+});
+</script>
 
 <?php wp_footer(); ?>
 
