852624232194ae78b28eb2541c9e2164ecffc1f8
Rajekevin <rajekevin@hotmail.fr>
2016-07-06 11:48:13 +0200

    MAJ 404 error page

--- 404.php
+++ 404.php
@@ -1,30 +1,72 @@
-<?php
+<header class="header">
+        <meta charset="utf-8"/>
+        <title><?php bloginfo('title')?></title>
+     <script src="assets/js/jquery-2.2.2.js"></script>
+     <link href="assets/css/animate.css" rel="stylesheet" media="screen">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
 
-get_header(); ?>
+    <link rel="stylesheet" href="assets/css/404.css" />
 
-
-
-	<div class="container">
-		<div class="row">
-			<div id="primary" >
-    			<article id="post-0" class="post error404 not-found">
-    				<i class="fa fa-frown-o"></i>
-    		    	<header>
-    		    	   	<h1 class="entry-title"><?php _e( '404 Error', 'arcade-basic' ); ?></h1>
-    		        </header>
-    		        <div class="entry-content description">
-    		            <p><?php _e( "Sorry. We can't seem to find the page you're looking for.", 'arcade-basic' ); ?></p>
-    		        </div>
-    		    </article>
-			</div>
-			<?php get_sidebar(); ?>
-		</div>
-	</div>
+       <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
 
 
+    <link href="404.css" rel="stylesheet" type="text/css"  media="all" />
+
+
+      
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+      
+        <?php wp_enqueue_script("jquery"); ?>
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+</header>
 
 
 
 
-<?php get_footer(); ?>
+
+    <!DOCTYPE HTMl>
+
+    <div id="plop">
+<html>
+
+<head>
+<title>404</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<link href='http://fonts.googleapis.com/css?family=Strait' rel='stylesheet' type='text/css'>
+</head>
+<body>
+     
+            <div class="wrap">
+                
+                <div class="banner"><img src="wp-content\themes\FoodByNight\img\404\fuel-404-logo.png" alt="" />
+                Cette page n'existe pas ;(</div>
+               
+           
+            </div>
+
+
+   
+       
+            
+</body>
+
+</html>   
+
+ </div>
+
+
+
+
+
+
+
